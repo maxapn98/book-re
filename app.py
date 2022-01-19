bc294395251e22aae1a580936b58f01b79dd9b19
maxapn98 <Nikolamerano@gmail.com>
2022-01-19 12:27:42 +0000

    Add register template and connection to mongodb

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import os
-from flask import Flask, render_template
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
+
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
 
 @app.route('/')
 def homepage():
@@ -35,6 +49,11 @@ def loginpage():
     Returns login page
     """
     return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def registerpage():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
