738e47adc1383b8ee9d70f8e54ae2c0b4d6c40f5
maxapn98 <Nikolamerano@gmail.com>
2022-01-21 10:55:36 +0000

    Add logout

--- app.py
+++ app.py
@@ -48,7 +48,18 @@ def librarypage():
 def profilepage(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html",username=username)
+
+    if session['user']:
+        return render_template("profile.html",username=username)
+        
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    flash("You have been logged out")
+    session.pop('user')
+    return redirect(url_for("loginpage"))
 
 
 @app.route('/login', methods=["GET", "POST"])
